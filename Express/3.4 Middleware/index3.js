c9eee4cef3f0db69d6ce5402dff524b414eef138
Nitigya Chandel <nitigyasingh27@gmail.com>
2025-02-12 15:21:26 +0530

    Custom Middleware

--- Express/3.4 Middleware/index3.js
+++ Express/3.4 Middleware/index3.js
@@ -3,12 +3,23 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger(req,res,next){
+  console.log("Requested URL: "+req.url);
+  console.log("Requested Method: "+req.method);
+  req.next();
+}
+
+// app.use(logger);
+
 app.use(logger);
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+app.post("/",(req,res)=>{
+  res.send("<h1>Hello World</h1>");
+});
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
